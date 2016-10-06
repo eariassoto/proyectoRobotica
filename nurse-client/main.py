94a9a175995d848e417204329afe1a13cd7e61ce
adminlab <adminaul@ECCI-LBL-202-01.ecci.ucr.ac.cr>
2016-10-06 14:15:30 -0600

    avances eervidaores

--- nurse-client/main.py
+++ nurse-client/main.py
@@ -2,7 +2,10 @@
 
 import sys
 from PyQt4.QtGui import *
+
 app = QApplication(sys.argv)
 button = QPushButton("Hello World", None)
 button.show()
+
+
 app.exec_()
--- soporte-vital-api/server.py
+++ soporte-vital-api/server.py
@@ -1,20 +1,36 @@
 #!/usr/bin/env python
 
+import threading
+import time
+from random import randint
+
+temp = 0;
+
 from flask import Flask, url_for
 app = Flask(__name__)
+
+def hilo_temp():
+    global temp
+    while True:
+        temp = 30 + randint(0,9)
+        time.sleep(5)
+
 
 @app.route('/')
 def api_root():
     return 'Welcome'
 
-@app.route('/articles')
+@app.route('/temperatura')
 def api_articles():
-    return 'List of ' + url_for('api_articles')
+    global temp
+    return 'Temperatura ' + str(temp) #'List of ' + url_for('api_articles')
 
 @app.route('/articles/<articleid>')
 def api_article(articleid):
     return 'You are reading ' + articleid
 
 if __name__ == '__main__':
-    app.run()
+    t = threading.Thread(target=hilo_temp)
+    t.start()
+    app.run(port=3000)
 
